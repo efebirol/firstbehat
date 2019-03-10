8d2dba4a5310e46810fc15431826119adaf48860
Birol <tw04n202@gmail.com>
2019-03-11 00:25:33 +0100

    Mehrere Repositores prüfen, ob sie existieren

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -269,5 +269,60 @@ class FeatureContext implements SnippetAcceptingContext
         //Response liefert ein 204 ("no content"), da Repository gelöscht wurde
         $this->iExpectAResponseCode(204);
     }
-    
+ 
+    /**
+     * @Given I have the following repositories:
+     */
+    public function iHaveTheFollowingRepositories(TableNode $table)
+    {
+        $this->table = $table->getRows();
+
+        // Drop the first element which is the header row
+        array_shift($this->table);
+
+        foreach($this->table as $id => $row) {
+            $this->table[$id]['name'] = $row[0] . '/' . $row[1];
+
+            $this->response = $this->client->get('/repos/' . $row[0] . '/' . $row[1]);
+
+            $this->iExpectAResponseCode(200);
+        }
+    }
+
+    /**
+     * @When I watch each repository
+     */
+    public function iWatchEachRepository()
+    {
+        $parameters = json_encode(['subscribed' => 'true']);
+
+        foreach($this->table as $row) {
+            $watch_url = '/repos/' . $row['name'] . '/subscription';
+            $this->client->put($watch_url, ['body' => $parameters]);
+        }
+    }
+
+    /**
+     * @Then My watch list will include those repositories
+     */
+    public function myWatchListWillIncludeThoseRepositories()
+    {
+        $watch_url = '/users/' . $this->username . '/subscriptions';
+        $this->response = $this->client->get($watch_url);
+        $watches = $this->getBodyAsJson();
+
+        foreach($this->table as $row) {
+            $fullname = $row['name'];
+
+            foreach($watches as $watch) {
+                if ($fullname == $watch['full_name']) {
+                    break 2;
+                }
+            }
+
+            throw new Exception("Error! " . $this->username . " is not watching " . $fullname);
+        }
+    }
+
+
 }
